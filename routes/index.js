8057148e3a2e5b3f2ddd17fe16bba87818f4d3db
diogo-febra-ferreira <DiogoFerreira_02@protonmail.com>
2023-12-13 15:04:36 +0100

    Set express.js template

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', (req, res) => {
+  res.send('Got a POST request')
+})
+
+router.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user')
+})
+
+router.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user')
+})
+
+module.exports = router;
