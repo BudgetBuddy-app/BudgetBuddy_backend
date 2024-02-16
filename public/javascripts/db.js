276eef3707d0cf30e13646c77ec7d5e895c9cd1e
Diogo Ferreira <DiogoFerreira_02@protonmail.com>
2024-02-16 11:26:59 +0100

    added loop to retry connection in case the DB is offline

--- public/javascripts/db.js
+++ public/javascripts/db.js
@@ -1,6 +1,7 @@
-const mysql = require('mysql2')
+const mysql = require('mysql2');
 require('dotenv').config();
 
+//TODO make DB password depend on .env? or on compose.yaml?
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
@@ -8,21 +9,29 @@ const pool = mysql.createPool({
     connectionLimit: (process.env.CONNECTION_LIMIT || 10)
 });
 
-pool.getConnection((err, connection) => {
-    if (err) {
-        console.error('Error getting connection:', err.stack);
-        return;
-    }
-    console.log('Connection obtained successfully');
-    // Select the database
-    connection.changeUser({ database: 'BudgetBuddyDB' }, (err) => {
+function getConnectionWithRetry() {
+    pool.getConnection((err, connection) => {
         if (err) {
-            console.error('Error selecting database:', err.stack);
-            return;
+            console.error('Error getting connection:', err.stack);
+            console.log('Retrying connection in  10 seconds...');
+            setTimeout(getConnectionWithRetry, 10000); // Retry after  10 seconds
+        } else {
+            console.log('Connection obtained successfully');
+            // Select the database
+            connection.changeUser({ database: 'BudgetBuddyDB' }, (err) => {
+                if (err) {
+                    console.error('Error selecting database:', err.stack);
+                    console.log('Retrying connection in  10 seconds...');
+                    setTimeout(getConnectionWithRetry, 10000); // Retry after  10 seconds
+                    return;
+                }
+                console.log('Database selected successfully');
+                pool.releaseConnection(connection);
+            });
         }
-        console.log('Database selected successfully');
-        pool.releaseConnection(connection);
     });
-});
+}
+
+getConnectionWithRetry(); // Start the connection attempt
 
 module.exports = pool;
